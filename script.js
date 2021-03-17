13b225c1310f4278bc54f33c308042f1c3f18bef
Mortivarus <58182213+Mortivarus@users.noreply.github.com>
2021-03-17 12:46:13 +0100

    Final draft of Yahtzee

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 const dieRow = document.getElementsByClassName("dieRow")
 const dieTotal = document.getElementsByClassName("dieTotal")
 const score = document.querySelectorAll(".score")
+const lockedClass = document.getElementsByClassName("locked")
+const oneClass = document.querySelectorAll(".one")
+const twoClass = document.querySelectorAll(".two")
+const totalPartOneBeforeBonus = document.getElementById("totalPartOneBeforeBonus")
+const bonus = document.getElementById("bonus")
+const totalPartOne = document.querySelectorAll(".totalPartOne")
+const totalPartTwo = document.getElementById("totalPartTwo")
+const total = document.getElementById("total")
 
 
-const throwCount = {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0
-}
-
-const compareArray = (array1, array2) => {
-    return JSON.stringify(array1) === JSON.stringify(array2) //Compare 2 arrays by stringifying them
-}
 
 // seqCompare checks for transitions from 1 to 1 or 1 to 2 or 2 to 1, and subsequently increases numSeq. As it only counts transitions, not consecutive numbers, 
 // numSeq needs to be adjusted by +1 in order to get consecutive numbers.  
@@ -40,10 +36,24 @@ const Throw = () => {
     
     const values = []
 
-    const lrgStrArray = [ //Array that contains all the options for a large street
-        [1, 1, 1, 1, 1, 0], // (1, 2, 3, 4, 5)
-        [0, 1, 1, 1, 1, 1] // (2, 3, 4, 5, 6)
-    ]
+    const locked = []
+
+    const throwCount = {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0
+    }
+
+    document.querySelectorAll('.tmpLocked').forEach(element => { // Change all tmpLock classes for locked classes 
+        element.classList.replace('tmpLocked', 'locked')
+    })
+
+    for(i = 0; i < lockedClass.length; i++){
+        locked[i] = lockedClass[i].innerHTML // Move all locked values to the 'locked' array
+    }
 
     for (i = 0; i < 5; i++){
         outcome.push(Math.floor(Math.random()*(6 -1 + 1)) +1)  // Calculate the dice values 1-6
@@ -57,7 +67,7 @@ const Throw = () => {
         const countNumber = outcome.filter(thrown => thrown == i).length; //Applies filter to array 'outcome' for number of throwCount, save length to a variable
         throwCount[i] = countNumber; //Append total of that number to the corresponding object key
     }
-    const throwCountValues = Object.values(throwCount)  //Move the object values of the throw to an array
+    const throwCountValues = Object.values(throwCount)  //Copy the object values of the throw to an array
 
     for (i = 0; i < throwCountValues.length; i++){
         values[i] = throwCountValues[i] * (i+1)  // Summarise the total score of the throw and add that to the HTML table
@@ -70,39 +80,62 @@ const Throw = () => {
         document.getElementById("carre").innerHTML = values.reduce((a,b) => a + b, 0)
     } else if(throwCountValues.includes(3) && throwCountValues.includes(2)){ //If array contains 3 times and 2 times the same value, add 25 points
         document.getElementById("fullHouse").innerHTML = 25
-    } else if(throwCountValues.includes(5)) { //If array contains 5 times the same value, add 50 points
-        document.getElementById("yahtzee").innerHTML = 50
+    } else if(throwCountValues.includes(5)) { 
+        document.getElementById("yahtzee").innerHTML = 50 //If array contains 5 times the same value, add 50 points
     } else if(seqCompare(throwCountValues) === 4){
         document.getElementById("smallStreet").innerHTML = 30 //If array contains 4 consecutive values, add 30 points
-    } else if(
-        compareArray(throwCountValues, lrgStrArray[1] ||
-        compareArray(throwCountValues, lrgStrArray[2]))
-    ){
+    } else if(seqCompare(throwCountValues) === 5){
         document.getElementById("largeStreet").innerHTML = 40 //If array contains 5 consecutive values, add 40 points
     } else{
         document.getElementById("chance").innerHTML = values.reduce((a,b) => a + b, 0) //Summarise the throw if there are no other categories
     }
-    
-    document.querySelectorAll('.tmpLocked').forEach(element => {
-        element.classList.replace('tmpLocked', 'locked')
-    })
 
+    for(i = 0; i < locked.length; i++){
+        lockedClass[i].innerHTML = locked[i] // Return all the locked class values to the table 
+    }  
 }
-// TODO: when clicking and thus locking a value, all final scores should be calculated. When the final score of part 1 is 63 points or more, a bonus of 35 should be added.
 
 document.getElementById("throwButton").addEventListener("click", Throw)
 
 document.querySelectorAll('.score').forEach(element => {
-    
-    element.addEventListener('click', (event) =>{
+    element.addEventListener('click', (event) =>{ // Add an event listener to every score value in order to be able to lock it.
+        const scoreOne = []
+        const scoreTwo = []
 
-        score.forEach(element => {
-            element.classList.remove("tmpLocked")
-        })
-        
-        event.target.classList.add("tmpLocked")
+        if(element.innerHTML !== '0'){ // Don't respond to empty cells
+            score.forEach(element => {
+                element.classList.remove("tmpLocked") // Make sure only one cell can be temporarily locked
+            })
 
-    })
+            event.target.classList.add("tmpLocked")
+
+            oneClass.forEach(element => { // Fill the scoreOne array with all locked or tmpLocked values of part one
+                if(element.classList.contains("tmpLocked") || element.classList.contains("locked")){
+                    scoreOne.push(parseInt(element.innerHTML))
+                }
+            })
+
+            twoClass.forEach(element =>{ // Fill the scoreTwo array with all locked or tmpLocked values of part two
+                if(element.classList.contains("tmpLocked") || element.classList.contains("locked")){
+                    scoreTwo.push(parseInt(element.innerHTML))
+                }
+            })
+
+            totalPartOneBeforeBonus.innerHTML = scoreOne.reduce((a,b) => a + b, 0) // Calculate the score for part one without the bonus
+
+            totalPartTwo.innerHTML = scoreTwo.reduce((a,b) => a + b, 0) // Calculate the score for part two
+       
+            if (parseInt(totalPartOneBeforeBonus.innerHTML) >= 63) { // If the total of part one is higher or equal to 63 points, add a bonus
+                bonus.innerHTML = 35
+            }
+
+            totalPartOne.forEach(element => {
+                element.innerHTML = parseInt(totalPartOneBeforeBonus.innerHTML) + parseInt(bonus.innerHTML) // Total the score of part one and the bonus
+            })
+
+            total.innerHTML = parseInt(totalPartOne[0].innerHTML) + parseInt(totalPartTwo.innerHTML) // Total the final score of part one and part two 
+        }
+    }) 
 })
 
 
