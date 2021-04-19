25048e3dfb85d9853180939085fb58f668098fb4
Mortivarus <58182213+Mortivarus@users.noreply.github.com>
2021-04-19 11:26:24 +0200

    Added supervisor corrections

--- script.js
+++ script.js
@@ -9,17 +9,26 @@ const bonus = document.getElementById("bonus")
 const totalPartOne = document.querySelectorAll(".totalPartOne")
 const totalPartTwo = document.getElementById("totalPartTwo")
 const total = document.getElementById("total")
-
-
+const threeOfAKind = document.getElementById("threeOfAKind")
+const carre = document.getElementById("carre")
+const fullHouse = document.getElementById("fullHouse")
+const yahtzee = document.getElementById("yahtzee")
+const smallStreet = document.getElementById("smallStreet")
+const largeStreet = document.getElementById("largeStreet")
+const chance = document.getElementById("chance")
+const throwButton = document.getElementById("throwButton")
 
 // seqCompare checks for transitions from 1 to 1 or 1 to 2 or 2 to 1, and subsequently increases numSeq. As it only counts transitions, not consecutive numbers, 
 // numSeq needs to be adjusted by +1 in order to get consecutive numbers.  
 
 const seqCompare = (array) => { //Checks an array for consecutive 1's and/or 2's
     let numSeq = 0
-    for(i = 0; i < array.length; i++){
-        j = i + 1 // Calculate j, which is one step ahead of i
-        k = Math.abs(array[i] - array[j])  // Calculate the difference between the current number and the one after that
+    for(let i = 0; i < array.length; i++){
+        let [j, k] = [
+            j = i + 1, // Calculate j, which is one step ahead of i
+            k = Math.abs(array[i] - array[j])  // Calculate the difference between the current number and the one after that
+        ]
+        
         if(k === 1 && array[i] !== 0 && array[j] !== 0 ){ // If the difference between the values is 1, and neither number is zero, increase numSeq by 1
             numSeq += 1 
         } else if(array[i] === array[j] && array[i] != 0){ // If both numbers are the same, and neither is zero, increase numSeq by 1
@@ -31,12 +40,13 @@ const seqCompare = (array) => { //Checks an array for consecutive 1's and/or 2's
     }  
 }
 
-const Throw = () => {
-    const outcome = []
-    
-    const values = []
+const throwIt = () => {
 
-    const locked = []
+    const [outcome, values, locked] = [
+        [],
+        [],
+        []
+    ]
 
     const throwCount = {
         1: 0,
@@ -51,43 +61,36 @@ const Throw = () => {
         element.classList.replace('tmpLocked', 'locked')
     })
 
-    for(i = 0; i < lockedClass.length; i++){
+    for(let i = 0; i < lockedClass.length; i++){
         locked[i] = lockedClass[i].innerHTML // Move all locked values to the 'locked' array
     }
 
-    for (i = 0; i < 5; i++){
-        outcome.push(Math.floor(Math.random()*(6 -1 + 1)) +1)  // Calculate the dice values 1-6
-    }
-
-    for (i = 0; i < outcome.length; i++){
+    for (let i = 0; i < 5; i++){
+        outcome.push(Math.floor(Math.random()*6 +1))  // Calculate the dice values 1-6 
         dieRow[i].innerHTML = outcome[i]  // Add the dice values to the HTML table
     }
 
     for (let i in throwCount) {
         const countNumber = outcome.filter(thrown => thrown == i).length; //Applies filter to array 'outcome' for number of throwCount, save length to a variable
         throwCount[i] = countNumber; //Append total of that number to the corresponding object key
-    }
-    const throwCountValues = Object.values(throwCount)  //Copy the object values of the throw to an array
-
-    for (i = 0; i < throwCountValues.length; i++){
-        values[i] = throwCountValues[i] * (i+1)  // Summarise the total score of the throw and add that to the HTML table
-        dieTotal[i].innerHTML = values[i]
+        let throwCountValues = Object.values(throwCount) //Copy the object values of the throw to an array
+        dieTotal[i].innerHTML = throwCountValues[i] * (i+1)  // Summarise the total score of the throw and add that to the HTML table
     }
 
     if (throwCountValues.includes(3)) { //If array contains 3 times the same value, summarise the total throw count
-        document.getElementById("threeOfAKind").innerHTML = values.reduce((a,b) => a + b, 0)
+        threeOfAKind.innerHTML = values.reduce((a,b) => a + b, 0)
     } else if(throwCountValues.includes(4)){ //If array contains 4 times the same value, summarise the total throw count
-        document.getElementById("carre").innerHTML = values.reduce((a,b) => a + b, 0)
+        carre.innerHTML = values.reduce((a,b) => a + b, 0)
     } else if(throwCountValues.includes(3) && throwCountValues.includes(2)){ //If array contains 3 times and 2 times the same value, add 25 points
-        document.getElementById("fullHouse").innerHTML = 25
+        fullHouse.innerHTML = 25
     } else if(throwCountValues.includes(5)) { 
-        document.getElementById("yahtzee").innerHTML = 50 //If array contains 5 times the same value, add 50 points
+        yahtzee.innerHTML = 50 //If array contains 5 times the same value, add 50 points
     } else if(seqCompare(throwCountValues) === 4){
-        document.getElementById("smallStreet").innerHTML = 30 //If array contains 4 consecutive values, add 30 points
+        smallStreet.innerHTML = 30 //If array contains 4 consecutive values, add 30 points
     } else if(seqCompare(throwCountValues) === 5){
-        document.getElementById("largeStreet").innerHTML = 40 //If array contains 5 consecutive values, add 40 points
+        largeStreet.innerHTML = 40 //If array contains 5 consecutive values, add 40 points
     } else{
-        document.getElementById("chance").innerHTML = values.reduce((a,b) => a + b, 0) //Summarise the throw if there are no other categories
+        chance.innerHTML = values.reduce((a,b) => a + b, 0) //Summarise the throw if there are no other categories
     }
 
     for(i = 0; i < locked.length; i++){
@@ -95,9 +98,9 @@ const Throw = () => {
     }  
 }
 
-document.getElementById("throwButton").addEventListener("click", Throw)
+throwButton.addEventListener("click", throwIt)
 
-document.querySelectorAll('.score').forEach(element => {
+score.forEach(element => {
     element.addEventListener('click', (event) =>{ // Add an event listener to every score value in order to be able to lock it.
         const scoreOne = []
         const scoreTwo = []
